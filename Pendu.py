a9d777bbbc90cff042e9fc92ee6cb67b7b719a4b
joris <jorisclement2@gmail>
2020-12-07 13:54:02 +0100

    version console 1.0

--- Pendu.py
+++ Pendu.py
@@ -1,9 +1,14 @@
+#@Copyright: Joris CLEMENT
 #Header
 """
 ce programme permet de jouer au pendu .
 Vous avez 8 essais pour découvrir le mot
 Auteur: Joris CLEMENT.
 date de réalisation: 23/11/2020
+"""
+"""
+TODO changer le noms des variables et fonction pour tout mettre en anglais.
+     faire une sécurtité de saisie
 """
 
 #Importation des bibliothéques
@@ -12,7 +17,7 @@ import random
 
 #Fonction nécessaires au programme
 
-from TP2_lib import randomWord,afficher,jouer
+from TP2_lib import randomWord,afficher,jouer,winLose
 
 
 
@@ -21,8 +26,8 @@ from TP2_lib import randomWord,afficher,jouer
 
 essai = 0
 mot = randomWord()
-
+print(mot)
 motFinal=(afficher(mot))
-print (motFinal)
-print(jouer(essai,mot,motFinal))
-
+print (motFinal[0])
+jouer=jouer(essai,mot,motFinal[0],motFinal[1])
+print(winLose(mot,jouer[0],jouer[1],jouer[2],motFinal[1]))
--- TP2_lib.py
+++ TP2_lib.py
@@ -1,48 +1,69 @@
+
+#@Copyright: Joris CLEMENT
 import random
 
+# la fonction randomWord permet de retouner un mot d'au moins 5 lettres provenant mesMots.txt.
 
 def randomWord():
     fich = open("mesMots.txt","r")
-    mot = random.choice(fich.readlines())
+    a=fich.readlines()
+    mot = random.choice(a)
     while (len(mot)-1) < 5:
-        mot = random.choice(fich.readlines())
+        mot = random.choice(a)
     fich.close()
     return mot
 
-
+# la fonction afficher permet retouner le mot a l'utilisateur sous la forme "e----" par exemple
+#si la premiere lettre se retrouve plusieurs fois dans le mot elle sera également afficher.
 def afficher(mot):
     mot=mot.lower()
     motFinal=''
+    lettre1=0
     for i in range (len(mot)-1):
         if mot[i] == mot[0]:
             motFinal += mot[i]
+            lettre1+=1
+            
+                
         else:
             motFinal += "-"
-    return motFinal
+       
+          
+
+    return motFinal,lettre1
 
 
-
-def jouer(essai,mot,motFinal):
-    a=list(motFinal)
-    while essai != 8 or '-' not in a:
+# la fonction jouer propose a l'utilisateur de saisir des lettres pour tenter de trouver le mot
+#l'utilisateur a le droit a 8 chances
+def jouer(essai,mot,motFinal,lettre1):
+    LmotFinal=list(motFinal)
+    win=0
+    while essai != 8 and (len(mot)-1)-lettre1 != win :
+        print(win,len(mot)-1,lettre1,(len(mot)-1)-lettre1,essai)
 
         lettre = input("saisir votre lettre : ")
         for i in range (len(mot)-1) :
             if mot[i]==lettre:
-                a[i] = lettre
-#                motFinal="".join(a)
-                
-                print(a)
+                LmotFinal[i] = lettre
+                win+=1       
+        print("".join(LmotFinal))
+        
                
         if lettre not in mot:
             essai += 1
             print( "il n'y a pas votre lettre dans le mot, il ne vous reste plus que ",8-essai,"chances")
-    return mot
+    return LmotFinal,essai,win
 
         
 
     
 
-
+def winLose(mot,LmotFinal,essai,win,lettre1):
+    print(win,len(mot),lettre1)
+    
+    if win == ((len(mot)-1)-lettre1):
+        return "vous avez gagné"
+    elif essai == 8: 
+        return "vous avez perdu, le mot était "+mot
 
 
